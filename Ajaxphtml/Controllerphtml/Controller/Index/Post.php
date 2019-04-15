7b84a85f1f21ec3bf0a32cc8ed5db77fae18ac6f
bthakkar96 <30663266+bthakkar96@users.noreply.github.com>
2019-04-15 10:02:04 +0530

    Add files via upload

---
+++ Ajaxphtml/Controllerphtml/Controller/Index/Post.php
@@ -0,0 +1,42 @@
+<?php
+namespace Ajaxphtml\Controllerphtml\Controller\Index;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\Result\JsonFactory;
+class Post extends \Magento\Framework\App\Action\Action
+{
+     /**
+     * @var Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+    protected $resultJsonFactory; 
+    /**
+     * @param Context     $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        JsonFactory $resultJsonFactory
+        )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->resultJsonFactory = $resultJsonFactory; 
+        return parent::__construct($context);
+    }
+    public function execute()
+    {
+        $height = $this->getRequest()->getParam('height');
+        $weight = $this->getRequest()->getParam('weight');
+        $result = $this->resultJsonFactory->create();
+        $resultPage = $this->resultPageFactory->create();
+        $block = $resultPage->getLayout()
+                ->createBlock('Ajaxphtml\Controllerphtml\Block\Index\Index')
+                ->setTemplate('Ajaxphtml_Controllerphtml::result.phtml')
+                ->setData('height',$height)
+                ->setData('weight',$weight)
+                ->toHtml();
+        $result->setData(['output' => $block]);
+        return $result;
+    } 
+}
